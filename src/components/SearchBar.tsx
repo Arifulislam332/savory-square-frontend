a0a1bb820cc78b55f9778866845b43606faa0220
Arifulislam332 <ayatullaharif332@gmail.com>
2024-05-03 17:12:20 +0600

    search api req

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form, FormControl, FormField, FormItem } from "./ui/form";
+import { Search } from "lucide-react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+const formSchema = z.object({
+  searchQuery: z.string({ required_error: "City name is required" }),
+});
+
+export type SearchForm = z.infer<typeof formSchema>;
+
+interface Props {
+  onSubmit: (formData: SearchForm) => void;
+  placeholder: string;
+  onReset?: () => void;
+}
+
+const SearchBar = ({ onSubmit, placeholder, onReset }: Props) => {
+  const form = useForm<SearchForm>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      searchQuery: "",
+    },
+  });
+
+  const handleReset = () => {
+    form.reset({
+      searchQuery: "",
+    });
+
+    if (onReset) {
+      onReset();
+    }
+  };
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-1 items-center gap-3 border-2 rounded-full p-3 mx-5"
+      >
+        <Search
+          size={30}
+          strokeWidth={2.5}
+          className="text-pink-500 hidden md:block ml-1"
+        />
+        <FormField
+          control={form.control}
+          name="searchQuery"
+          render={({ field }) => (
+            <FormItem className="flex-1">
+              <FormControl>
+                <Input
+                  {...field}
+                  placeholder={placeholder}
+                  required
+                  className="border-none shadow-none md:text-xl focus-visible:ring-0"
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        {form.formState.isDirty && (
+          <Button
+            onClick={handleReset}
+            type="button"
+            variant="outline"
+            className="rounded-full"
+          >
+            clear
+          </Button>
+        )}
+
+        <Button type="submit" className="rounded-full bg-pink-500">
+          Search
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default SearchBar;
